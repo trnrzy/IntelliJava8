9b9ecec2579b21f558e4ea0828c97661270c8e4c
trnrzy <tehran.rzayev@gmail.com>
2015-10-18 18:23:38 +0200

    Initial commit - IntelliJava8

---
+++ src/main/java/MyMain.java
@@ -0,0 +1,10 @@
+/**
+ * Created by PC-TEHRAN on 18/10/2015.
+ */
+public class MyMain {
+
+    public static void main (String [] args){
+        System.out.println("HelloWorld from IntelliJ and Java 8");
+
+    }
+}
